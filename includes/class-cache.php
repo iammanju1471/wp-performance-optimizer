60077a5c4bd465c82bcd18256ad25094869d5cb4
Manjunath G <75680771+iammanju1471@users.noreply.github.com>
2026-03-10 20:55:45 +0530

    Create class-cache.php

---
+++ includes/class-cache.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class WPO_Cache_Manager {
+
+    public function enable_cache() {
+        // caching logic
+    }
+
+    public function clear_cache() {
+        // clear cache logic
+    }
+
+}
